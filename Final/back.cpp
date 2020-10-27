5a29a313afe59c44498bee1509ca47c3fbd78d32
Hamper-X <dasmat2000@gmail.com>
2020-10-27 14:27:29 -0300

    start

--- Final/back.cpp
+++ Final/back.cpp
@@ -1,5 +1,10 @@
 
-
+/*
+    Sequencial:
+        real    0m10,550s
+        user    0m0,015s
+        sys     0m0,016s
+*/
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -31,23 +36,31 @@ double f_theta(double x) {
 }
 
 void train(double x, double y) {
-	
+	int k;
+
+    #pragma omp target map(tofrom:W[:N]) //map(to:c[:N]) map(to:W[:N])
+    #pragma omp teams distribute parallel for simd reduction(+:W)
 	for (int i = 0; i < N; i++) {
-		W[i] = W[i] - epsilon * 2 * (f_theta(x) - y) * V[i] * x * 
-               (1 - sigmoid(c[i] + W[i] * x)) * sigmoid(c[i] + W[i] * x);
+		W[i] = W[i] - epsilon * 2 * (f_theta(x) - y) * V[i] * x * (1 - sigmoid(c[i] + W[i] * x)) * sigmoid(c[i] + W[i] * x);
 	}
+   // #pragma omp target map(tofrom:V[:N]) map(to:c[:N]) map(to:W[:N])
+   // #pragma omp teams distribute parallel for simd 
 	for (int i = 0; i < N; i++) {
-		V[i] = V[i] - epsilon * 2 * (f_theta(x) - y) * sigmoid(c[i] + W[i] * x);
+        k = epsilon * 2 * (f_theta(x) - y) * sigmoid(c[i] + W[i] * x);
+		V[i] = V[i] - k;
 	}
 	b = b - epsilon * 2 * (f_theta(x) - y);
-	for (int i = 0; i < N; i++) {
-		c[i] = c[i] - epsilon * 2 * (f_theta(x) - y) * V[i] * 
-               (1 - sigmoid(c[i] + W[i] * x)) * sigmoid(c[i] + W[i] * x);
+	//#pragma omp target map(tofrom:c[:N]) map(to:W[:N]) map(to:V[:N])
+    //#pragma omp teams distribute parallel for simd private(k) reduction(+:V) schedule(dynamic,100)
+    for (int i = 0; i < N; i++) {
+        k = epsilon * 2 * (f_theta(x) - y) * V[i] * (1 - sigmoid(c[i] + W[i] * x)) * sigmoid(c[i] + W[i] * x);
+		c[i] = c[i] - k;
 	}
 }
 
 int main() {
 	srand(time(NULL));
+     
 	for (int i = 0; i < N; i++) {
 		W[i] = 2 * rand() / RAND_MAX -1;
 		V[i] = 2 * rand() / RAND_MAX -1;
@@ -61,7 +74,7 @@ int main() {
 		trainSet[i] = make_pair(i * 2 * PI / Train_Set_Size, sin(i * 2 * PI / Train_Set_Size));
 	}
 
-	#pragma omp parallel for num_threads(8)
+	//#pragma omp parallel for num_threads(8)
 	for (int j = 0; j < epoch; j++) {
 		for (int i = 0; i < Train_Set_Size; i++) {
 			train(trainSet[i].first, trainSet[i].second);
@@ -80,10 +93,10 @@ int main() {
 	}
 
 
-	//Neural Network Approximate f(x) = sin(x) -> Red Graph
+	/*//Neural Network Approximate f(x) = sin(x) -> Red Graph
 	for (int k = 0; k < x.size(); k++) {
 		printf("%f %f %f \n", x[k],y1[k],y2[k]);
 	}
-	printf("e\n");
+	printf("e\n");*/
 	return 0;
 }
